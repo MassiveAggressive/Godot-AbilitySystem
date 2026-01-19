3d1fc8cf59c101cf2f098b1b4efd2e3f488070e5
massive <omer.hatay0534@gmail.com>
2026-01-19 19:23:18 +0300

    AttributeModifierData güncellendi.

--- Core/AbilitySystem/AttributeSystem/AttributeModifierData/AttributeModifierData.gd
+++ Core/AbilitySystem/AttributeSystem/AttributeModifierData/AttributeModifierData.gd
@@ -1,42 +1,96 @@
 @tool
 class_name AttributeModifierData extends Resource
 
-@export var attribute: String:
+var attribute: String:
 	set(value):
 		attribute = value
-@export var operator: Util.EOperator
-
-@export_group("Modifier Magnitude")
-@export var magnitude_type: Util.EMagnitudeType:
+var operator: Util.EOperator
+var magnitude_type: Util.EMagnitudeType:
 	set(value):
 		magnitude_type = value
 		notify_property_list_changed()
 
-@export var coefficient: float = 1.0
+var coefficient: float = 1.0
 
 var scalable_float_magnitude: float
 
 var source_attribute: String
 var source_attribute_source: Util.EAttributeSource
 
-func _validate_property(property: Dictionary) -> void:
-	if property["name"] == "attribute":
-		var attribute_strings: Array[String]
-		
-		for attribute_set_name: String in AttributePicker.editor_attributes:
-			for attribute_name in AttributePicker.editor_attributes[attribute_set_name]:
-				attribute_strings.append(attribute_set_name + "." + attribute_name)
-		
-		property["type"] = TYPE_STRING
-		property["usage"] = PROPERTY_USAGE_DEFAULT
-		property["hint"] = PROPERTY_HINT_ENUM
-		property["hint_string"] = ",".join(attribute_strings)
-
 func _get_property_list() -> Array[Dictionary]:
 	var properties: Array[Dictionary]
 	
+	var attribute_strings: Array[String]
+	
+	for attribute_set_name: String in AttributePicker.editor_attributes:
+		for attribute_name in AttributePicker.editor_attributes[attribute_set_name]:
+			attribute_strings.append(attribute_set_name + "." + attribute_name)
+	
+	properties.append(
+		{
+			"name": "attribute",
+			"type": TYPE_STRING,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(attribute_strings)
+		}
+	)
+	
+	var operator_array: Array[String]
+	for key in Util.EOperator.keys():
+		key = key as String
+		operator_array.append(key.to_lower().replace("_", " ").capitalize())
+	
+	properties.append(
+		{
+			"name": "operator",
+			"type": TYPE_INT,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(operator_array)
+		}
+	)
+	
+	properties.append(
+				{
+					"name": "coefficient",
+					"type": TYPE_FLOAT,
+					"usage": PROPERTY_USAGE_DEFAULT
+				}
+	)
+	
+	properties.append(
+		{
+			"name": "Modifier Magnitude",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_CATEGORY
+		}
+	)
+	
+	var magnitude_type_array: Array[String]
+	for key in Util.EMagnitudeType.keys():
+		key = key as String
+		magnitude_type_array.append(key.to_lower().replace("_", " ").capitalize())
+	
+	properties.append(
+		{
+			"name": "magnitude_type",
+			"type": TYPE_INT,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(magnitude_type_array)
+		}
+	)
+	
 	match magnitude_type:
 		Util.EMagnitudeType.SCALABLE_FLOAT:
+			properties.append(
+				{
+					"name": "Scalable Float Magnitude",
+					"type": TYPE_NIL,
+					"usage": PROPERTY_USAGE_SUBGROUP
+				}
+			)
 			properties.append(
 				{
 					"name": "scalable_float_magnitude",
@@ -52,13 +106,6 @@ func _get_property_list() -> Array[Dictionary]:
 					"usage": PROPERTY_USAGE_SUBGROUP
 				}
 			)
-			
-			var attribute_strings: Array[String]
-		
-			for attribute_set_name: String in AttributePicker.editor_attributes:
-				for attribute_name in AttributePicker.editor_attributes[attribute_set_name]:
-					attribute_strings.append(attribute_set_name + "." + attribute_name)
-			
 			properties.append(
 				{
 					"name": "source_attribute",
@@ -69,18 +116,17 @@ func _get_property_list() -> Array[Dictionary]:
 				}
 			)
 			
-			var attribute_source_strings: Array[String]
-			
+			var source_attribute_source_array: Array[String]
 			for key in Util.EAttributeSource.keys():
-				attribute_source_strings.append((key as String).capitalize())
-			
+				key = key as String
+				source_attribute_source_array.append(key.to_lower().replace("_", " ").capitalize())
 			properties.append(
 				{
 					"name": "source_attribute_source",
 					"type": TYPE_INT,
 					"usage": PROPERTY_USAGE_DEFAULT,
 					"hint": PROPERTY_HINT_ENUM,
-					"hint_string": ",".join(attribute_source_strings)
+					"hint_string": ",".join(source_attribute_source_array)
 				}
 			)
 	
